02a7581f3720528ef641c37363947c69c8635089
os2021 <os2021@os2021>
2021-10-14 14:14:59 +0200

    feat: implement vmprint

--- kernel/exec.c
+++ kernel/exec.c
@@ -107,7 +107,7 @@ exec(char *path, char **argv)
     if(*s == '/')
       last = s+1;
   safestrcpy(p->name, last, sizeof(p->name));
-    
+
   // Commit to the user image.
   oldpagetable = p->pagetable;
   p->pagetable = pagetable;
@@ -115,6 +115,9 @@ exec(char *path, char **argv)
   p->trapframe->epc = elf.entry;  // initial program counter = main
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
+  //print the pagetable only of the first process
+  if(p->pid == 1)
+    vmprint(pagetable);
 
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,33 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+//print the content of page table
+void
+print_pte(pagetable_t pagetable, int level)
+{
+  //check every pte in actual level
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    //check if pte is valid
+    if(PTE_V & pte){
+      for(int j = 0; j <= level; j++)
+	printf(".. ");
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    }
+    //check if there is a deeper level
+    //if yes, jump to that level
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      uint64 child = PTE2PA(pte);
+      print_pte((pagetable_t)child, level+1);
+    }
+  }
+}
+
+//vmprint is called to print content of page table
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  print_pte(pagetable, 0);
+}
